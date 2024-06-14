55dcccc43882df94927ad0f9647597dac5b6301f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-14 11:40:00 +0100

    Apply styling to the watchlist elements.

--- assets/js/display-watchlist.js
+++ assets/js/display-watchlist.js
@@ -4,7 +4,7 @@ const watchlistEl = document.querySelector("#watchlist");
 // GLOBAL VARIABLES
 const TMDB_API_KEY = "7b928560fcfa8991abeaa28e946a0252";
 const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_BASE_IMG_URL = "https://image.tmdb.org/t/p/w300";
+const TMDB_BASE_IMG_URL = "https://image.tmdb.org/t/p/w200";
 
 let responseArr = [];
 
@@ -61,12 +61,12 @@ function displaySavedMedia() {
 
     const titleEl = document.createElement("h4");
     const ratingEl = document.createElement("p");
-    const removeBtnEl = document.createElement("button");
+    // const removeBtnEl = document.createElement("button");
     const posterImgEl = document.createElement("img");
 
     titleEl.textContent = title;
     ratingEl.textContent = `⭐ ${rating}`;
-    removeBtnEl.textContent = "Remove";
+    // removeBtnEl.textContent = "Remove";
     posterImgEl.src = `${TMDB_BASE_IMG_URL}/${posterPathURL}`;
     posterImgEl.alt = title;
 
@@ -74,11 +74,25 @@ function displaySavedMedia() {
     savedMediaImgEl.append(posterImgEl);
     savedMediaInfoEl.append(titleEl);
     savedMediaInfoEl.append(ratingEl);
-    savedMediaInfoEl.append(removeBtnEl);
+    // savedMediaInfoEl.append(removeBtnEl);
 
     savedMediaEl.append(savedMediaImgEl);
     savedMediaEl.append(savedMediaInfoEl);
 
     watchlistEl.append(savedMediaEl);
+
+    posterImgEl.setAttribute("style", "border-radius: 10px");
+    savedMediaInfoEl.setAttribute(
+      "style",
+      "display: flex; flex-direction: column; align-items: center; width: 200px"
+    );
+    titleEl.setAttribute(
+      "style",
+      "margin-top: 10px; text-align: center; font-size: 1.25rem"
+    );
+    ratingEl.setAttribute(
+      "style",
+      "color: grey; margin-top: 10px; font-size: 1.1rem"
+    );
   }
 }
