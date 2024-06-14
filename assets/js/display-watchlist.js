48c528a868c10fec850f96c198fae25658d9cefe
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-14 11:09:26 +0100

    Add functionality to display saved media.

--- assets/js/display-watchlist.js
+++ assets/js/display-watchlist.js
@@ -1,3 +1,6 @@
+// DOM ELEMENTS
+const watchlistEl = document.querySelector("#watchlist");
+
 // GLOBAL VARIABLES
 const TMDB_API_KEY = "7b928560fcfa8991abeaa28e946a0252";
 const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
@@ -39,3 +42,43 @@ async function fetchMediaDetails() {
 }
 
 fetchMediaDetails();
+
+function displaySavedMedia() {
+  console.log(responseArr);
+
+  for (let i = 0; i < responseArr.length; i++) {
+    // store data to be displayed in to variables
+    const savedMedias = responseArr;
+
+    const title = savedMedias[i].title;
+    const rating = savedMedias[i].vote_average;
+    const posterPathURL = savedMedias[i].poster_path;
+
+    // Create DOM elements
+    const savedMediaEl = document.createElement("div");
+    const savedMediaImgEl = document.createElement("div");
+    const savedMediaInfoEl = document.createElement("div");
+
+    const titleEl = document.createElement("h4");
+    const ratingEl = document.createElement("p");
+    const removeBtnEl = document.createElement("button");
+    const posterImgEl = document.createElement("img");
+
+    titleEl.textContent = title;
+    ratingEl.textContent = `⭐ ${rating}`;
+    removeBtnEl.textContent = "Remove";
+    posterImgEl.src = `${TMDB_BASE_IMG_URL}/${posterPathURL}`;
+    posterImgEl.alt = title;
+
+    // Add content to the DOM elements
+    savedMediaImgEl.append(posterImgEl);
+    savedMediaInfoEl.append(titleEl);
+    savedMediaInfoEl.append(ratingEl);
+    savedMediaInfoEl.append(removeBtnEl);
+
+    savedMediaEl.append(savedMediaImgEl);
+    savedMediaEl.append(savedMediaInfoEl);
+
+    watchlistEl.append(savedMediaEl);
+  }
+}
