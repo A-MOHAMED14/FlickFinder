47c9ec7bfc6cbd8ff420791c846d1e2c231571cc
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-17 10:50:06 +0100

    Refactor trending media display to separate function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,6 @@ function handleSearchFormSubmit(event) {
 
 searchFormEl.addEventListener("submit", handleSearchFormSubmit);
 
-fetchTrending();
-
 function fetchTrending() {
   const apiURL = `${TMDB_API_BASE_URL}/trending/all/week?api_key=${TMDB_API_KEY}`;
 
@@ -37,42 +35,48 @@ function fetchTrending() {
     })
     .then((data) => {
       console.log(data);
-      const trendingArr = data.results;
-
-      for (let i = 0; i < trendingArr.length; i++) {
-        const posterPath = trendingArr[i].poster_path;
-        const title = trendingArr[i].title || trendingArr[i].name;
-        const mediaType = trendingArr[i].media_type;
-
-        const posterEl = document.createElement("img");
-        const trendingCard = document.createElement("div");
-        const titleEl = document.createElement("p");
-        const mediaTypeEl = document.createElement("p");
-
-        posterEl.src = `${TMDB_BASE_IMG_URL}${posterPath}`;
-        posterEl.alt = title;
-        titleEl.textContent = title;
-        mediaTypeEl.textContent = mediaType;
-
-        trendingCard.append(posterEl);
-        trendingCard.append(titleEl);
-        trendingCard.append(mediaTypeEl);
-
-        trendingWeekEl.append(trendingCard);
-
-        trendingCard.setAttribute(
-          "style",
-          "width: 200px;text-align: center; margin: 30px"
-        );
-
-        posterEl.setAttribute("style", "border-radius: 10px");
-
-        titleEl.setAttribute("style", "margin-top: 10px");
-
-        mediaTypeEl.setAttribute(
-          "style",
-          "font-size: 1.2rem; color: grey; margin-top: 5px"
-        );
-      }
+      displayTrending(data);
     });
+}
+
+fetchTrending();
+
+function displayTrending(data) {
+  const trendingArr = data.results;
+
+  for (let i = 0; i < trendingArr.length; i++) {
+    const posterPath = trendingArr[i].poster_path;
+    const title = trendingArr[i].title || trendingArr[i].name;
+    const mediaType = trendingArr[i].media_type;
+
+    const posterEl = document.createElement("img");
+    const trendingCard = document.createElement("div");
+    const titleEl = document.createElement("p");
+    const mediaTypeEl = document.createElement("p");
+
+    posterEl.src = `${TMDB_BASE_IMG_URL}${posterPath}`;
+    posterEl.alt = title;
+    titleEl.textContent = title;
+    mediaTypeEl.textContent = mediaType;
+
+    trendingCard.append(posterEl);
+    trendingCard.append(titleEl);
+    trendingCard.append(mediaTypeEl);
+
+    trendingWeekEl.append(trendingCard);
+
+    trendingCard.setAttribute(
+      "style",
+      "width: 200px;text-align: center; margin: 30px"
+    );
+
+    posterEl.setAttribute("style", "border-radius: 10px");
+
+    titleEl.setAttribute("style", "margin-top: 10px");
+
+    mediaTypeEl.setAttribute(
+      "style",
+      "font-size: 1.2rem; color: grey; margin-top: 5px"
+    );
+  }
 }
